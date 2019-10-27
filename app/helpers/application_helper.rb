e86cbb4cd272321b6831cac181415b6416660177
tompng <tomoyapenguin@gmail.com>
2019-10-28 00:44:14 +0900

    use webpack & typescript

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def javascript_webpack_tag(name)
+    javascript_include_tag '/' + webpack_manifest[name], skip_pipeline: true
+  end
+
+  def webpack_manifest
+    return @webpack_manifest if @webpack_manifest && Rails.env.production?
+    @webpack_manifest = JSON.load Rails.root.join('public', 'assets', 'webpack-manifest.json')
+  end
 end
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const LiveReloadPlugin = require('webpack-livereload-plugin')
+
+module.exports = (_, { mode }) => {
+  const isProduction = mode === 'production'
+  const dist = path.resolve(__dirname, 'public/assets')
+
+  return {
+    entry: ['./client/main.ts'],
+    output: {
+      filename: '[name].[contenthash].js',
+      path: dist,
+      publicPath: 'assets/'
+    },
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+      rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new ManifestPlugin({ fileName: 'webpack-manifest.json' }),
+      !isProduction && new LiveReloadPlugin({ appendScriptTag: true })
+    ].filter(Boolean)
+  }
+}
