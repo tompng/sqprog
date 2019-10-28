f73e1d4fe8863a867f05c3e9d68a8e0282d6b2b4
tompng <tomoyapenguin@gmail.com>
2019-10-28 20:15:27 +0900

    highlightjs

---
+++ client/lib/highlight.ts
@@ -0,0 +1,54 @@
+import { highlight } from 'highlight.js'
+
+const alias: Record<string, string> = {
+  tsx: 'jsx'
+}
+
+function highlightLines(lang: string, code: string) {
+  lang = alias[lang] || lang
+  let html: string
+  try {
+    html = highlight(lang, code).value
+  } catch {
+    html = code
+  }
+  const lines = splitIntoLines(html)
+  return lines.map(line =>
+    line.map(
+      ({ text, classes }) => {
+        if (classes.length) {
+          return `<span class="${classes.join(' ')}">${text}</span>`
+        } else {
+          return `<span>${text}</span>`
+        }
+      }
+    ).join('') || '<span>\n</span>'
+  )
+}
+
+function splitIntoLines(html: string) {
+  const classes: string[] = []
+  let currentLine: { text: string; classes: string[] }[] = []
+  const lines = [currentLine]
+  const matches = html.match(/<span class="[^"]+">|<\/span>|[^<]+/g)
+  if (!matches) return []
+  matches.forEach(seg => {
+    if (seg[0] == '<') {
+      if (seg[1] == '/') {
+        classes.pop()
+      } else {
+        classes.push(seg.match(/<span class="([^"]+)">/)![1])
+      }
+    } else {
+      const ls = seg.split('\n')
+      ls.forEach((text, i) => {
+        if (text) currentLine.push({ text, classes: [...classes] })
+        if (i + 1 < ls.length) lines.push(currentLine = [])
+      })
+    }
+  })
+  if (lines[lines.length - 1].length === 0) lines.pop()
+  return lines
+}
+
+export default highlightLines
