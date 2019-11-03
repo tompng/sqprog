26ecdebfa41f45758ce50c6720609fc0657f29f8
tompng <tomoyapenguin@gmail.com>
2019-11-04 01:54:29 +0900

    vote icons

--- client/lib/ikachan.ts
+++ client/lib/ikachan.ts
@@ -1,6 +1,71 @@
 const coords = [{x:0.972,y:0},{x:0.952,y:0.098},{x:0.904,y:0.186},{x:0.843,y:0.265},{x:0.775,y:0.339},{x:0.704,y:0.409},{x:0.63,y:0.477},{x:0.555,y:0.543},{x:0.478,y:0.608},{x:0.4,y:0.671},{x:0.32,y:0.732},{x:0.239,y:0.79},{x:0.154,y:0.844},{x:0.066,y:0.891},{x:-0.029,y:0.924},{x:-0.128,y:0.924},{x:-0.206,y:0.864},{x:-0.247,y:0.773},{x:-0.274,y:0.676},{x:-0.334,y:0.602},{x:-0.433,y:0.589},{x:-0.533,y:0.593},{x:-0.634,y:0.601},{x:-0.734,y:0.607},{x:-0.833,y:0.602},{x:-0.926,y:0.567},{x:-0.974,y:0.484},{x:-0.934,y:0.394},{x:-0.867,y:0.322},{x:-0.935,y:0.25},{x:-0.976,y:0.161},{x:-0.934,y:0.072},{x:-0.867,y:0},{x:-0.934,y:-0.072},{x:-0.976,y:-0.161},{x:-0.935,y:-0.25},{x:-0.867,y:-0.322},{x:-0.934,y:-0.394},{x:-0.974,y:-0.484},{x:-0.926,y:-0.567},{x:-0.833,y:-0.602},{x:-0.734,y:-0.607},{x:-0.634,y:-0.601},{x:-0.533,y:-0.593},{x:-0.433,y:-0.589},{x:-0.334,y:-0.602},{x:-0.274,y:-0.676},{x:-0.247,y:-0.773},{x:-0.206,y:-0.864},{x:-0.128,y:-0.924},{x:-0.029,y:-0.924},{x:0.066,y:-0.891},{x:0.154,y:-0.844},{x:0.239,y:-0.79},{x:0.32,y:-0.732},{x:0.4,y:-0.671},{x:0.478,y:-0.608},{x:0.555,y:-0.543},{x:0.63,y:-0.477},{x:0.704,y:-0.409},{x:0.775,y:-0.339},{x:0.843,y:-0.265},{x:0.904,y:-0.186},{x:0.952,y:-0.098}]
-// JSON.stringify(coords.map(c=>({x:Math.round((c.x-0.3)/1.3*1000)/1000,y:Math.round(c.y/1.3*1000)/1000}))).replace(/"/g,'')
+const eyeOuterCoords = [{x:0.031,y:0},{x:0.077,y:0.246},{x:-0.077,y:0.5},{x:-0.4,y:0.5},{x:-0.538,y:0.246},{x:-0.492,y:0},{x:-0.538,y:-0.246},{x:-0.4,y:-0.5},{x:-0.077,y:-0.5},{x:0.077,y:-0.246}]
+const eyeCircleOuter = { x: -0.215, y: 0.231, r: 0.262 }
+const eyeCircleInner = { x: -0.169, y: 0.231, r: 0.169 }
+const svgOpenTag = '<svg width="100px" height="100px" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+const svgCloseTag = '</svg>'
 type Point = { x: number; y: number }
+
+function IconGood() {
+  svgOpenTag
+  return svgOpenTag + `
+    <path fill="#4f4" d="${coordsToPath(coords, { rotate: -Math.PI / 2, scale: 1 })}" />
+    <path fill="#464" d="${coordsToPath(eyeOuterCoords, { rotate: -Math.PI / 2, scale: 1 })}" />
+    <circle fill="#afa" cx="${50 - 50 * eyeCircleOuter.y}" cy="${50 - 50 * eyeCircleOuter.x}" r="${50 * eyeCircleOuter.r}" />
+    <circle fill="#afa" cx="${50 + 50 * eyeCircleOuter.y}" cy="${50 - 50 * eyeCircleOuter.x}" r="${50 * eyeCircleOuter.r}" />
+    <circle fill="#464" cx="${50 - 50 * eyeCircleInner.y}" cy="${50 - 50 * eyeCircleInner.x}" r="${50 * eyeCircleInner.r}" />
+    <circle fill="#464" cx="${50 + 50 * eyeCircleInner.y}" cy="${50 - 50 * eyeCircleInner.x}" r="${50 * eyeCircleInner.r}" />
+    <circle fill="#afa" cx="42" cy="56" r="2" />
+    <circle fill="#afa" cx="64" cy="56" r="2" />
+  ` + svgCloseTag
+}
+
+function IconBad() {
+  return svgOpenTag + `
+    <path fill="#f44" d="${coordsToPath(coords, { rotate: Math.PI / 2, scale: 1 })}" />
+    <path fill="#844" d="${coordsToPath(eyeOuterCoords, { rotate: Math.PI / 2, scale: 1 })}" />
+    <g transform="translate(50,38) rotate(24)">
+      <rect fill="#400" x="-22" y="-3" width="44" height="6" />
+    </g>
+    <g transform="translate(50,38) rotate(-24)">
+      <rect fill="#400" x="-22" y="-3" width="44" height="6" />
+    </g>
+  ` + svgCloseTag
+}
+
+function IconForward() {
+  const outLoc = { x: 0.1, y: 0, rotate: Math.PI / 6, scale: 0.9 }
+  const inLoc = { x: 0.1, y: 0, rotate: -Math.PI / 6, scale: 0.6 }
+  return svgOpenTag + `
+    <path fill="#8af" d="${coordsToPath(coords, outLoc)}" />
+    <path fill="#fab" d="${coordsToPath(coords, inLoc)}" />
+    <path fill="#cad" d="${coordsToPath(eyeOuterCoords, inLoc)}" />
+  ` + svgCloseTag
+}
+
+function IconRotate() {
+  const rotcoords = coords.map(rot)
+  const roteyes = eyeOuterCoords.map(rot)
+  function rot({ x, y }: Point) {
+    const r = (y + 3) / 2
+    const th = x / 1.8
+    return {
+      x: r * Math.cos(th) - 1,
+      y: r * Math.sin(th)
+    }
+  }
+  const loc1 = { x: 0, y: 0, rotate: 2 * Math.PI / 3, scale: 1 }
+  const loc2 = { x: 0, y: 0, rotate: 5 * Math.PI / 3, scale: 1 }
+  return svgOpenTag + `
+    <path fill="#fb0" d="${coordsToPath(rotcoords, loc1)}" />
+    <path fill="#fd0" d="${coordsToPath(roteyes, loc1)}" />
+    <path fill="#ff0" d="${coordsToPath(rotcoords, loc2)}" />
+    <path fill="#fd0" d="${coordsToPath(roteyes, loc2)}" />
+  ` + svgCloseTag
+}
+
+
+(window as any).svg = IconGood() + IconBad() + IconForward() + IconRotate()
 
 function coordsToPath(baseCoords: Point[], { x: baseX, y: baseY, rotate, scale }: { x?: number; y?: number; rotate?: number; scale?: number } = {} ) {
   const cos = Math.cos(rotate || 0)
@@ -136,8 +201,7 @@ export function genIconSVG(seed: string='0123456789abcdef') {
       `<path d="${path}" fill="${col}" />`
     )
   }
-  const svghead = '<svg width="100px" height="100px" version="1.1" xmlns="http://www.w3.org/2000/svg">'
-  return svghead + paths.join('') + '</svg>'
+  return svgOpenTag + paths.join('') + svgCloseTag
 }
 
 export function randomString(){
@@ -152,7 +216,7 @@ export function applyRippleStyle() {
   document.head.appendChild(style)
   style.textContent=`
   .MuiTouchRipple-child{
-    background-image: url('data:image/svg+xml,<svg width="100px" height="100px" version="1.1" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="${path}"/></svg>');
+    background-image: url('data:image/svg+xml,${svgOpenTag}<path fill="rgba(255, 255, 255, 0.5)" d="${path}"/>${svgCloseTag}');
     background-size: contain;
     border-radius: 0;
   }
